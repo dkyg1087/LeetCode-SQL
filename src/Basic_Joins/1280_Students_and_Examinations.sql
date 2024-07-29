a1b454903b253d23440d98b97823f744d4ccd919
ToolzMing <derek246810@gmail.com>
2024-07-29 14:33:22 -0700

    add 1280

---
+++ src/Basic_Joins/1280_Students_and_Examinations.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT Students.student_id,Students.student_name,Subjects.subject_name,COUNT(Examinations.subject_name) AS attended_exams
+FROM Subjects CROSS JOIN Students LEFT JOIN Examinations ON Students.student_id = Examinations.student_id AND subjects.subject_name = Examinations.subject_name
+GROUP BY Students.student_id,Students.student_name,Subjects.subject_name
+ORDER BY Students.student_id,Subjects.subject_name
